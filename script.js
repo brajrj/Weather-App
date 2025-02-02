c8c21c6dfa3fae8f583015bd63f988f3add71b23
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-02-02 12:27:40 +0530

    update code

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 
-const apiKey = "";
-const apiUrl= "";
-
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=&{apiKey}`);
-    var data = await response.json();
-
-    console.log(data);
-
-    document.querySelector(".city").innerHTML = 
-    
-}
-
-
-
-checkWeather();
