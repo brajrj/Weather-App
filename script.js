b32347ecdb58498bebd69e94805910701eccca07
bs <shekhawatbrajraj888@gmail.com>
2025-02-02 12:03:18 +0530

    Added new files: index.html,script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const apiKey = "";
+const apiUrl= "";
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=&{apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = 
+    
+}
+
+
+
+checkWeather();
